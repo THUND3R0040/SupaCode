80e4a6e4d1c66ea56511503c7b3b3f29d8e31aa9
Ahmed Gargouri <ahmed.gargouri@insat.ucar.tn>
2024-12-02 19:18:53 +0100

    last one

--- CodeTogetherFront/src/socket.js
+++ CodeTogetherFront/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.VITE_API_URI;
+const URL = import.meta.env.VITE_API_URI;
 export const socket = io(URL, {
   reconnectionDelay: 5000,
 });
